d5a23df01375c846af3fd6e97c0891b5b4a5120f
pat cor <patricia.corduant@gmail.com>
2021-01-18 17:00:25 +0100

    exos

---
+++ exe1-0.js
@@ -0,0 +1,3 @@
+console.log(10 + 5);
+console.log(85473-5556);
+console.log(8772.5*2);
---
+++ exe1-2.js
@@ -0,0 +1,4 @@
+let nickname = "Gerard";
+let Name = "Lambert";
+let city = "Paris";
+console.log("My name is " + nickname +" "+ Name + " and I live in " + city);
