c23df97d4b7dbf6ffb69a2e93d8bc3c865d80485
pat cor <patricia.corduant@gmail.com>
2021-01-19 14:09:12 +0100

    testexe2-2

---
+++ exe2-2.js
@@ -0,0 +1,12 @@
+const readlineSync = require("readline-sync");
+
+let min = readlineSync.question("Your min number ? ");
+let max = readlineSync.question("your max number ? ");
+let current = readlineSync.question("Your current number ? ");
+   if (((Number(current)) >= (Number(min)) ) && ((Number(current)) <= (Number(max))))  { 
+    console.log(current + " is between " + min + " and " + max) }
+   // else if ( min > max ) {
+    //    alert ("You dont understand anything !!! Exit now !!!");} 
+    else {
+    console.log(current + " is not between " + min + " and " + max)
+}
