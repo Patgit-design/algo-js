15b5bb18b67e9b382912479a2e5c89f5a299b294
pat cor <patricia.corduant@gmail.com>
2021-01-20 14:25:46 +0100

    exe4-3

---
+++ exe4-3.js
@@ -0,0 +1,23 @@
+const readlineSync = require("readline-sync");
+
+let min = 1;
+let max = 10;
+let arr = [];
+let i = 1;
+let n = readlineSync.question("How much random will you? ");
+
+function rand10(min, max) {
+
+return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function multiRand(n)  {
+    while (i <= n) {
+      rand10(min, max);
+      i += 1;
+      arr.push(rand10(min, max));
+    }}
+    multiRand(n);
+ 
+    
+    console.log("Your random number is: " + arr);
