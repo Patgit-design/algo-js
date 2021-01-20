923e858a1b44564570ee0a6d57b81502e900c529
pat cor <patricia.corduant@gmail.com>
2021-01-20 11:06:49 +0100

    exe3-4

---
+++ exe3-4.js
@@ -0,0 +1,8 @@
+let Arr = [3, 4, 5, 10, 50];
+let arrMax =  Math.max(...Arr);
+  console.log(arrMax);
+
+
+  let Arm = [3, 4, 5, 10, 50];
+  let arrMin =  Math.min(...Arm);
+    console.log(arrMin); 
