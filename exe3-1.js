a462eaa0d167f70066593b5c9576251992da0ab2
pat cor <patricia.corduant@gmail.com>
2021-01-20 10:15:48 +0100

    corr

--- exe3-1.js
+++ exe3-1.js
@@ -6,7 +6,7 @@
 
 //console.log(sum);
 
-let a = [1, 2, 3, 4, 5];
-let sum = a.reduce(function(a, b) { return a + b; }, 0);
+let arr = [1, 2, 3, 4, 5];
+let sum = arr.reduce(function(a, b) { return a + b; }, 0);
 
 console.log(sum);
