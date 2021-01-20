de422883dceca439c9fd3d0ba8e3d12562d60e1e
pat cor <patricia.corduant@gmail.com>
2021-01-20 10:46:15 +0100

    corr

--- exe3-3.js
+++ exe3-3.js
@@ -1,6 +1,16 @@
-let oldArr = [3, 1, 5, 2, 9];
-let newArr = [];
-for (let i=0; i < oldArr.length; i++) {
-    newArr.push(oldArr[i]);}
+//let oldArr = [3, 1, 5, 2, 9];
+//let newArr = [];
+//for (let i=0; i < oldArr.length; i++) {
+//    newArr.push(oldArr[i]);}
 
-    console.log(oldArr + "," + newArr);
+  //  console.log(oldArr + "," + newArr);
+
+   // let oldArr = [3, 1, 5, 2, 9]; /*spread */
+   // let newArr = [...oldArr];
+
+   // console.log(oldArr + "," + newArr);
+
+let oldArr = [3, 1, 5, 2, 9];
+let newArr = oldArr.slice();     //slice pour une partie de array dans ce cas vide = tout
+
+console.log(oldArr + "," + newArr);
