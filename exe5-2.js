14fa5c03d8d1cc01021c1dc07abdcd642d4d69c0
pat cor <patricia.corduant@gmail.com>
2021-01-21 14:21:09 +0100

    exe5-2.js

---
+++ exe5-2.js
@@ -0,0 +1,24 @@
+const readlineSync = require("readline-sync");
+let answer = { name: '', prod: '', cast: [] };
+let members = '';
+let x=0;
+
+function askTvSerie() {
+    answer.name = readlineSync.question("What is the name of your favorite TV serie? ");
+    answer.prod = readlineSync.question("What is his production year ?");
+    while (members != 'end') {
+        members = readlineSync.question("Which are the names of the cast members ? Write end to stop !");
+        answer.cast.push(members);
+    }}
+    answer.cast.pop(x-1);
+
+    function randomizeCast(tvSerie)  {
+        return answer.cast[Math.floor(Math.random()*answer.length)];
+       
+    }
+    askTvSerie();
+
+   
+    
+
+console.log(JSON.stringify(randomizeCast(tvSerie)));
